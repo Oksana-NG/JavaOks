e9856879e3e821b277b6268c2dbe8554fb6e3e37
Oksana-NG <67648874+Oksana-NG@users.noreply.github.com>
2020-07-09 13:25:28 +0300

    IDEA wokrspace

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("New Text ");
+        System.out.println("Branch 2");
+	// write your code here
+    }
+}
