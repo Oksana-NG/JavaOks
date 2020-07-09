1289ea067fe88cf3541fd8466e989daf2a2821b8
Oksana-NG <67648874+Oksana-NG@users.noreply.github.com>
2020-07-09 13:51:57 +0300

    Update Main.java
    Branch 1 - edited.
    Branch 2 - to delete.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("New Text ");
-        System.out.println("Branch 2");
+        System.out.println("Branch 1");
+	System.out.println("Delete Branch 2");
 	// write your code here
     }
 }
